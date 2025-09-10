10d2d2be64a750672143e78830535ad1ce6735d7
vithyasekar2212-crypto <vithya.sekar2212@gmail.com>
2025-09-10 17:44:02 +0530

    Add files via upload

---
+++ vithya.py
@@ -0,0 +1,15 @@
+#append
+x=[1,2,3,4]
+x.append(5)
+print(x)
+#extend
+x=[1,"bad",5.3]
+x.extend("map")
+print(x)
+#length
+abc=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(len(abc))
+
+
+
+
