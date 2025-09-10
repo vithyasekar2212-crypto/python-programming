11cc4d6efc1689db919ef028d84dce36f573f721
vithyasekar2212-crypto <vithya.sekar2212@gmail.com>
2025-09-10 17:47:59 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,12 @@
+a=[]
+n=int(input("enter the numbers:"))
+for i in range(0,n):
+    m=int(input("enter a number:"))
+    a.append(m)
+print(a)
+print(a[0])
+print(a[-1])
+print(a[0:3])
+print(a[-3:])
+print(a[1:5]) 
+
