caf985ccaebdc4fa92a1d7f141a32ac2cfa2adcf
vithyasekar2212-crypto <vithya.sekar2212@gmail.com>
2025-09-18 17:13:11 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,12 @@
+class stud:
+    def __init__(self,a,b,c):
+        self.sno=a
+        self.sname=b
+        self.sage=c
+    def display(self):
+        print(self.sno)
+        print(self.sname)
+        print(self.sage)
+ob=stud(123,'abc',20)
+ob.display()
+        
